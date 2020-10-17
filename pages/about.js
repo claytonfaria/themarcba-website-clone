e29d226a83c639d6c6d90e902558938b68a5a523
Clayton Faria <claytonfaria@gmail.com>
2020-10-17 19:55:40 +0800

    work experience component created

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import { Box, Heading, Image, Flex, Text, Icon } from '@chakra-ui/core';
 import { FaLaptopCode } from 'react-icons/fa';
 
+import WorkExperience from '../components/about/workExperience';
+
 export default function About({ colorScheme }) {
-  const { primaryTextColor, highlightColor } = colorScheme;
+  const { primaryTextColor } = colorScheme;
   return (
     <Box>
       <Flex>
@@ -37,68 +39,14 @@ export default function About({ colorScheme }) {
         Work experience{' '}
         <Icon verticalAlign="-15%" fontSize="2.2rem" as={FaLaptopCode} />
       </Heading>
-      <Flex direction="column" marginTop="0.8rem" marginBottom="1.4rem">
-        <Flex alignItems="center">
-          <Box
-            width="1rem"
-            position="relative"
-            marginRight="1.5rem"
-            height="1rem"
-            display="flex"
-          >
-            <Box
-              width="100%"
-              position="absolute"
-              height="100%"
-              display="inline-flex"
-              borderWidth="2px"
-              borderRadius="full"
-              borderColor={highlightColor}
-              animation="ping 1s cubic-bezier(0, 0, 0.2, 1) infinite"
-            />
-            <Box
-              width="1rem"
-              height="1rem"
-              borderWidth="2px"
-              display="inline-flex"
-              position="relative"
-              borderRadius="full"
-              borderColor={highlightColor}
-            />
-          </Box>
-          <Box flex="1">
-            <Text fontWeight="500" textColor={primaryTextColor}>
-              July 2017 - Current · Luxembourg 🇱🇺
-            </Text>
-            <Heading fontSize="1.4rem">
-              Freelancing full-stack web developer
-            </Heading>
-            <Text
-              textColor={primaryTextColor}
-              fontWeight="700"
-              fontSize="1.2rem"
-            >
-              Marc Backes Software Labs
-            </Text>
-          </Box>
-        </Flex>
-      </Flex>
-
-      <style global jsx>
-        {`
-          @keyframes ping {
-            0% {
-              transform: scale(1);
-              opacity: 1;
-            }
-            75%,
-            100% {
-              transform: scale(2);
-              opacity: 0;
-            }
-          }
-        `}
-      </style>
+      <WorkExperience
+        colorScheme={colorScheme}
+        employment={{
+          company: 'Marc Backes Labs',
+          dateAndCountry: 'July 2017 - Current · Luxembourg 🇱🇺',
+          position: 'Freelancing',
+        }}
+      />
     </Box>
   );
 }
