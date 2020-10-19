32b3bcffd253ae6e7a5012971ca09cc7fe066902
Clayton Faria <claytonfaria@gmail.com>
2020-10-19 09:13:05 +0800

    added page transition animation

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,4 +1,5 @@
 import { Container, Flex } from '@chakra-ui/core';
+import { motion } from 'framer-motion';
 import { cloneElement } from 'react';
 
 import { projects } from '../../data';
@@ -7,24 +8,39 @@ import Footer from './footer';
 import Header from './header';
 import PageMetadata from './pageMetadata';
 
-export default function Layout({ children }) {
+export default function Layout({ children, router }) {
+  const MotionContainer = motion.custom(Container);
+
   const siteTitle =
     'Web Developer | Clayton Faria | JavaScript | React.js, Next.js, Node.js';
+
+  const pageTransitionAnimation = {
+    pageAnimate: {
+      opacity: 1,
+    },
+    pageInitial: {
+      opacity: 0,
+    },
+  };
 
   return (
     <>
       <PageMetadata siteTitle={siteTitle} />
       <Header />
       <Flex direction="column" minHeight="90vh">
-        <Container
+        <MotionContainer
           maxWidth={['768px', '768px', '768px', '1024px']}
           marginBottom={['6rem', '0']}
           paddingX={5}
           marginX="auto"
           flex="1"
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={pageTransitionAnimation}
         >
           {cloneElement(children, { projects })}
-        </Container>
+        </MotionContainer>
         <BottomNavigation />
         <Footer />
       </Flex>
--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,11 @@ import Layout from '../components/layout';
 import { CustomColorProvider } from '../context/colorContext';
 import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
       <CustomColorProvider>
-        <Layout>
+        <Layout router={router}>
           <Component {...pageProps} />
         </Layout>
       </CustomColorProvider>
